24813880603e401bb01b6d3d11429cb3e7f349ca
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2021-05-31 21:21:33 +0200

    create game dialog
    And move the dialogs to seperate files.

---
+++ src/dialogs/GamesDialog.js
@@ -0,0 +1,203 @@
+import { useRef, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Clear } from '@material-ui/icons';
+import { Button, IconButton, Dialog, DialogTitle, DialogContent, Autocomplete, TextField, List, ListItem, ListItemText, Box } from '@material-ui/core';
+import swr, { mutate } from 'swr';
+import formatDuration from 'format-duration';
+
+import Loading from '../Loading.js';
+import { fetcher, parseDuration, formatGame } from '../util.js';
+
+const useStyles = makeStyles({
+	addGame: {
+		display: 'flex',
+
+		'& > :first-child': {
+			width: '100%',
+		},
+
+		'& > :not(:first-child)': {
+			marginLeft: '15px',
+		},
+
+		'& > :last-child': {
+			marginLeft: '25px',
+		},
+	},
+	createGame: {
+		display: 'flex',
+
+		'& > *:not(:last-child)': {
+			marginRight: '10px',
+		},
+	}
+});
+
+async function addGame(dict) {
+	await fetch(`http://local.lieuwe.xyz:6070/games`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(dict),
+	});
+	mutate('http://local.lieuwe.xyz:6070/games');
+}
+
+const CreateGameDialog = props => {
+	const nameRef = useRef(null);
+	const platformRef = useRef(null);
+
+	const classes = useStyles();
+
+	const handleClose = () => props.handleClose();
+	const onAdd = () => {
+		const name = nameRef.current.value;
+		const platform = platformRef.current.value;
+
+		const dict = { name };
+		if (platform != null && platform.length > 0) {
+			dict['platform'] = platform;
+		}
+		addGame(dict).catch(e => console.error(e));
+
+		handleClose();
+	};
+
+	return (
+		<Dialog
+			open={props.open}
+			onClose={handleClose}
+			fullWidth={true}
+			maxWidth="sm"
+		>
+			<DialogTitle>Nieuwe game aanmaken</DialogTitle>
+			<DialogContent>
+				<div className={classes.createGame}>
+					<TextField
+						required
+						id="name"
+						label="Naam"
+						inputRef={nameRef}
+					/>
+
+					<TextField
+						id="platform"
+						label="Platform"
+						inputRef={platformRef}
+					/>
+
+					<Button onClick={onAdd}>Toevoegen</Button>
+				</div>
+			</DialogContent>
+		</Dialog>
+	);
+};
+
+const GamesDialogItem = props => {
+	const game = props.game;
+
+	const onClick = () => props.onSeek(game.start_time);
+
+	return (
+		<ListItem>
+			<ListItemText primary={game.name} secondary={game.platform} />
+			<Button onClick={onClick}>{formatDuration(1000 * game.start_time)}</Button>
+			<IconButton onClick={() => props.onDelete(game.id)}>
+				<Clear />
+			</IconButton>
+		</ListItem>
+	);
+};
+
+const AddGameRow = props => {
+	const time = formatDuration(1000 * props.currentTime);
+
+	const classes = useStyles();
+
+	const { data, error } = swr('http://local.lieuwe.xyz:6070/games', fetcher);
+	const selectedInfo = useRef({ game: null, time });
+
+	const onAdd = () => {
+		const curr = selectedInfo.current;
+		props.onAdded({ ...curr.game, start_time: parseDuration(curr.time) });
+	};
+
+	if (error) {
+		return "Error while loading persons";
+	} else if (!data) {
+		return <Loading />;
+	}
+
+	return (
+		<div className={classes.addGame}>
+			<Autocomplete
+				options={data}
+				getOptionLabel={formatGame}
+				renderOption={(props, game) => (
+					<li {...props}>
+						<ListItemText primary={game.name} secondary={game.platform} />
+					</li>
+				)}
+				renderInput={params => <TextField {...params} label="Game" />}
+				getOptionSelected={(a, b) => a.id === b.id}
+				onChange={(_, game) => selectedInfo.current.game = game}
+			/>
+			<TextField
+				required
+				defaultValue={time}
+				label="Tijd"
+				onChange={e => selectedInfo.current.time = e.target.value}
+			/>
+			<Button onClick={onAdd}>Toevoegen</Button>
+		</div>
+	);
+}
+
+export default function GamesDialog(props) {
+	const [items, setItems] = useState(props.video.games);
+	const changed = useRef(false);
+
+	const [createDialogOpen, setCreateDialogOpen] = useState(false);
+
+	const onAdded = info => {
+		const newItems = [...items, info];
+		newItems.sort((a, b) => a.start_time < b.start_time);
+		setItems(newItems);
+		changed.current = true;
+	};
+	const onDelete = id => {
+		setItems(items.filter(g => g.id !== id));
+		changed.current = true;
+	};
+	const handleClose = () => props.handleClose(changed.current ? items : null, null);
+	const onSeek = progress => props.handleClose(null, progress);
+
+	const nodes = items.map((g, i) => <GamesDialogItem key={i} game={g} onDelete={onDelete} onSeek={onSeek} />);
+
+	return (
+		<>
+			<Dialog
+				open={true}
+				onClose={handleClose}
+				fullWidth={true}
+				maxWidth="sm"
+			>
+				<DialogTitle>Games in deze stream</DialogTitle>
+				<DialogContent>
+					<List>
+						{nodes}
+					</List>
+
+					<AddGameRow currentTime={props.currentTime} onAdded={onAdded} />
+
+					<div style={{ textAlign: 'center', marginTop: '15px' }}>
+						<Button variant="contained" onClick={() => setCreateDialogOpen(true)}>Nieuwe game aanmaken</Button>
+					</div>
+				</DialogContent>
+			</Dialog>
+
+			<CreateGameDialog open={createDialogOpen} handleClose={() => setCreateDialogOpen(false)} />
+		</>
+	);
+};
---
+++ src/dialogs/PersonsDialog.js
@@ -0,0 +1,73 @@
+import {  Dialog, DialogTitle, DialogContent, Autocomplete, TextField } from '@material-ui/core';
+import { useRef } from 'react';
+import swr from 'swr';
+
+import Loading from '../Loading.js';
+import { fetcher } from '../util.js';
+
+function sortFunc(a, b) {
+	const a_is_top = [1, 2].includes(a.id);
+	const b_is_top = [1, 2].includes(b.id);
+
+	if (a_is_top === b_is_top) {
+		return b.name - a.name;
+	} else if (a_is_top) {
+		return -1;
+	} else {
+		return 1;
+	}
+}
+
+export default function PersonsDialog(props) {
+	const selected = useRef(props.video.persons);
+	const changed = useRef(false);
+
+	const { data, error } = swr('http://local.lieuwe.xyz:6070/persons', fetcher);
+	if (data != null) {
+		data.sort(sortFunc);
+	}
+
+	let content = <Loading />;
+	if (error) {
+		content = "Error while loading persons";
+	} else if (data) {
+		content = <Autocomplete
+			onChange={(_, items) => {
+				selected.current = items;
+				changed.current = true;
+			}}
+			multiple
+			options={data}
+			getOptionLabel={({ name }) => name}
+			defaultValue={props.video.persons}
+			getOptionSelected={(a, b) => a.id === b.id}
+			filterSelectedOptions
+			renderInput={(params) => (
+				<TextField
+					{...params}
+					label="Personen"
+					placeholder="Personen"
+				/>
+			)}
+		/>;
+	}
+
+	const handleClose = () => {
+		selected.current.sort((a, b) => a.id < b.id);
+		props.handleClose(changed.current ? selected.current : null, null);
+	};
+
+	return (
+		<Dialog
+			open={true}
+			onClose={handleClose}
+			fullWidth={true}
+			maxWidth="sm"
+		>
+			<DialogTitle>Personen in deze stream</DialogTitle>
+			<DialogContent>
+				{content}
+			</DialogContent>
+		</Dialog>
+	);
+};
