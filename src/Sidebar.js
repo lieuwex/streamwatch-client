34dfc1b268e7ef89928dc800c17a90e37cfb0804
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2021-10-26 21:01:44 +0200

    reenable ChatManager cleaning after 3 seconds offset

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -150,13 +150,13 @@ const Chat = React.memo(props => {
 	}, []);
 
 	const messages = useRef([]);
-	//const previousOffset = useRef(null);
+	const previousOffset = useRef(null);
 	useEffect(() => {
-		//const dist = Math.abs(props.offset - previousOffset.current);
-		//if (dist >= 3000) {
-		//	// if more than 3 seconds, clear all messages from the manager
-		//	manager.current.clear();
-		//}
+		const dist = Math.abs(props.offset - previousOffset.current);
+		if (dist >= 3000) {
+			// if more than 3 seconds, clear all messages from the manager
+			manager.current.clear();
+		}
 
 		const actualTimestamp = props.offset + props.video.timestamp*1e3;
 		let currTimestamp = actualTimestamp;
@@ -179,7 +179,7 @@ const Chat = React.memo(props => {
 		msgs = msgs.slice(Math.max(0, msgs.length - 300), msgs.length);
 
 		messages.current = msgs;
-		//previousOffset.current = props.offset;
+		previousOffset.current = props.offset;
 	});
 
 	const chatRef = useRef(null);
