ee6d841914aae049b1d8153272ca2e7b39f4adbf
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2022-02-22 20:57:52 +0100

    update to React rc

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import App from './App';
 
 import migrate from './migrations.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+	<App />
+	</React.StrictMode>,
 );
 
 window.requestIdleCallback(() => {
