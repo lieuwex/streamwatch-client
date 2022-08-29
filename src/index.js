lieuwex/streamwatch-client

commit 3d3cfdddada62c70a970d75bc833d5e1e1be3be8

add optional style choices

src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './styleOptions.css';
 import App from './App';
 
 import migrate from './migrations.js';
@@ -15,3 +16,10 @@ root.render(
 window.requestIdleCallback(() => {
 	migrate().catch(e => console.error(e));
 });
+
+window.requestIdleCallback(() => {
+	const json = JSON.parse(localStorage.getItem('styleOptions'));
+	for (const className of json) {
+		document.body.classList.add(className);
+	}
+})

src/styleOptions.css
@@ -1,0 +1,9 @@
+/* === no-timestamps === */
+
+body.no-timestamps .message-timestamp {
+	display: none !important;
+}
+
+body.no-timestamps .sidebar.visible {
+	width: 350px !important;
+}

commit 16bd266f2d196da21415fb880647973353461b4f

fix error being logged to console introduced by 3d3cfdddada62c70a970d75bc833d5e1e1be3be8

src/index.js
@@ -18,7 +18,7 @@ window.requestIdleCallback(() => {
 });
 
 window.requestIdleCallback(() => {
-	const json = JSON.parse(localStorage.getItem('styleOptions'));
+	const json = JSON.parse(localStorage.getItem('styleOptions') || '[]');
 	for (const className of json) {
 		document.body.classList.add(className);
 	}