b69ce19dd1b40708234d2292cf105d3a61d98e35
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2022-08-29 18:19:38 +0200

    Revert "escape html characters"
    This reverts commit 372c2b1d78ad9dcf3c11773c391721cf9aec2bf6.
    
    If a chat message ended with an emote, this commit would make it so that
    the last character of the emoticon was not included in the emoticon but
    stayed in the message as-is.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -49,23 +49,20 @@ function parseEmotes(emoteString) {
 
 	return emotes;
 }
-function convertEmotes(str, deltas, emotes) {
+function convertEmotes(str, emotes) {
 	const res = [];
 	let last = 0;
 
 	for (const emote of emotes) {
-		const emoteStart = deltas.indexOf(emote.start);
-		const emoteEnd = deltas.lastIndexOf(emote.end);
-
-		if (last < emoteStart) {
-			res.push(str.slice(last, emoteStart));
+		if (last < emote.start) {
+			res.push(str.slice(last, emote.start));
 		}
 
-		const content = str.slice(emoteStart, emoteEnd);
+		const content = str.slice(emote.start, emote.end);
 		const { url, width, height } = getTwitchEmoticon(emote.id);
 		res.push(`<img alt="${content}" title="${content}" src="${url}" width="${width}" height="${height}" decoding="sync"/>`);
 
-		last = emoteEnd;
+		last = emote.end;
 	}
 
 	if (last < str.length) {
@@ -95,32 +92,12 @@ function convertUrls(str) {
 	return res.join('');
 }
 
-function encode(str) {
-	const deltas = [];
-	let res = '';
-
-	for (let i = 0; i < str.length; i++) {
-		const c = str[i];
-		const esc = ({
-			'&': '&amp;',
-			'<': '&lt;',
-			'>': '&gt;',
-		})[c] || c;
-
-		res += esc;
-		for (let x = 0; x < esc.length; x++) {
-			deltas.push(i);
-		}
-	}
-
-	return [deltas, res];
-}
-
 const ChatMessage = React.memo(props => {
 	// HACK: we temp removed that encoding step, this is unsafe. (5474481e09f)
-	let [deltas, body] = encode(props.message.message);
+	//let body = encode(props.message.message);
+	let body = props.message.message;
 	body = convertUrls(body);
-	body = convertEmotes(body, deltas, parseEmotes(props.message.tags.emotes || ''));
+	body = convertEmotes(body, parseEmotes(props.message.tags.emotes || ''));
 
 	let color;
 	let fontColor;
