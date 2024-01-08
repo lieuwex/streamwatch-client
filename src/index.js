94eb2af7ce363da949fdaecb791db0c9f4980f39
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2024-01-08 12:32:15 +0100

    redirect to new url if using old one

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import App from './App';
 
 import migrate from './migrations.js';
 
+if (window.location.hostname === 'local.lieuwe.xyz') {
+	const url = window.location.href.replace('http://local.lieuwe.xyz:6070/', 'https://streams.lieuwe.xyz/');
+	window.location.replace(url);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
