613c9303232d85e4f0218ab6ca9df35e4c46872e
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2024-12-12 23:22:27 +0100

    introDurations: fix stupid logic error
    When watching a stream which has a einde LW from time 0, the intro
    button appears but doesn't really skip to anything useful.
    This button that appears at time [0, introDuration] is meant for streams
    without a einde LW.
    Make this explicit so the button only shows up when it does something
    remotely useful.

--- src/introDurations.js
+++ src/introDurations.js
@@ -8,15 +8,15 @@ export function getSkipTo(video, progressSecs) {
 	const introDuration = getIntroDuration(video.id); // seconds
 	const lekkerWachtens = video.games.filter(g => g.id === 7);
 
+	if (lekkerWachtens.length === 0 && progressSecs < introDuration) {
+		return introDuration;
+	}
+
 	for (let i = lekkerWachtens.length - 1; i >= 0; i--) {
 		const start = lekkerWachtens[i].start_time;
 		if (progressSecs > start && progressSecs < (start + introDuration)) {
 			return start + introDuration;
 		}
-	}
-
-	if (progressSecs < introDuration) {
-		return introDuration;
 	}
 
 	return null;
